eaaff83923ae2f6146c0d62843cb72f4992173e0
zhi_heng <zhi_heng@hunliji.com>
2021-12-22 12:59:06 +0800

    更新打包配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
+  history: {
+    type: 'hash',
+  },
+  base: './',
+  publicPath: './',
+  hash: true,
+  dva: {
+    hmr: true,
+  },
   nodeModulesTransform: {
     type: 'none',
   },
   routes: [{ path: '/', component: '@/pages/index' }],
   fastRefresh: {},
-  publicPath: './',
 });
